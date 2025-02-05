8589888db98ce5f97f2195dce8d97c66d2594450
LuthfiAbid <luthfiabid27@gmail.com>
2025-02-05 17:03:10 +0700

    interface, simple request

---
+++ http/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, error := http.Get("http://google.com")
+	if error != nil {
+		fmt.Println("Error : ", error)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+}
